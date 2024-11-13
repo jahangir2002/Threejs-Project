068b84fabf7bd4e62b19d32dc2b3a2fe0c8021fa
jahangir2002 <jahangirshaikh553@gmail.com>
2024-11-13 16:08:52 +0530

    making nav bar and adding gsap

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 import './style.css';
 import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+// import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader';
 import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer.js';
 import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass.js';
 import { RGBShiftShader } from 'three/examples/jsm/shaders/RGBShiftShader.js';
 import { ShaderPass } from 'three/examples/jsm/postprocessing/ShaderPass.js';
+import gsap from 'gsap';
 
 // Scene
 const scene = new THREE.Scene();
@@ -56,16 +57,45 @@ const renderPass = new RenderPass(scene, camera);
 composer.addPass(renderPass);
 
 const rgbShiftPass = new ShaderPass(RGBShiftShader);
-rgbShiftPass.uniforms['amount'].value = 0.00010;
+rgbShiftPass.uniforms['amount'].value = 0.00025;
 composer.addPass(rgbShiftPass);
 
 // Controls
-const controls = new OrbitControls(camera, renderer.domElement);
-controls.enableDamping = true;
+// const controls = new OrbitControls(camera, renderer.domElement);
+// controls.enableDamping = true;
+// controls.dampingFactor = 0.025; // The dampingFactor is a parameter commonly used in physics simulations and animations to control the "smoothness" of a transition
+
+window.addEventListener('mousemove', (e) => {
+
+  if (model) { 
+    const rotationX = (e.clientX / window.innerWidth - 0.5) * Math.PI * .3; // to get the mouse position and set the rotation of the model
+    const rotationY = (e.clientY / window.innerHeight - 0.5) * Math.PI * .3; 
+    gsap.to(model.rotation, {
+      x: rotationY,
+      y: rotationX,
+      z: 0,
+      duration: 0.5,
+      ease: "power2.out"
+    }); // Smoothly animate rotation with GSAP
+
+    // model.rotation.y = rotationX; // to set the rotation of the model
+    // model.rotation.x = rotationY; // to set the rotation of the model
+    
+  }
+
+  
+});
+
+// responsive more like 
+window.addEventListener('resize', () => {
+  renderer.setSize(window.innerWidth, window.innerHeight); // to update the renderer size
+  camera.aspect = window.innerWidth / window.innerHeight; // to update the camera aspect ratio
+  camera.updateProjectionMatrix(); // to update the camera aspect ratio
+});
 
 function animate() {
   window.requestAnimationFrame(animate);
-  controls.update();
+  // controls.update(); // to make the camera move smoothly
   
   if (model) {
     // model.rotation.y += 0.005;
