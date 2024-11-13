1772cee8fb57235c88b50043374aaf489b01f152
jahangir2002 <jahangirshaikh553@gmail.com>
2024-11-13 14:54:59 +0530

    Post Processing add

--- main.js
+++ main.js
@@ -3,19 +3,23 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader';
+import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer.js';
+import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass.js';
+import { RGBShiftShader } from 'three/examples/jsm/shaders/RGBShiftShader.js';
+import { ShaderPass } from 'three/examples/jsm/postprocessing/ShaderPass.js';
 
 // Scene
 const scene = new THREE.Scene();
 // Camera
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100);
-camera.position.z = 5;
+const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 100);
+camera.position.z = 6;
 
 // HDRI Environment
 const rgbeLoader = new RGBELoader();
 rgbeLoader.load('https://dl.polyhaven.org/file/ph-assets/HDRIs/hdr/1k/pond_bridge_night_1k.hdr', function(texture) {
   texture.mapping = THREE.EquirectangularReflectionMapping;
   scene.environment = texture;
-  scene.background = texture;
+  // scene.background = texture;
 });
 
 // GLTF Loader
@@ -35,32 +39,39 @@ loader.load('./DamagedHelmet.gltf', (gltf) => {
 const renderer = new THREE.WebGLRenderer({ 
   canvas: document.querySelector("#canvas"),
   antialias: true,
+  alpha: true, // to make the background transparent
 });
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2)); // to get great performance without secrificing resorces
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.toneMapping = THREE.ACESFilmicToneMapping; // to get a better look
+renderer.toneMapping = THREE.ACESFilmicToneMapping;
 renderer.toneMappingExposure = 1;
 renderer.outputEncoding = THREE.sRGBEncoding;
 
 const pmremGenerator = new THREE.PMREMGenerator(renderer);
 pmremGenerator.compileEquirectangularShader();
 
+// Post Processing
+const composer = new EffectComposer(renderer);
+const renderPass = new RenderPass(scene, camera);
+composer.addPass(renderPass);
+
+const rgbShiftPass = new ShaderPass(RGBShiftShader);
+rgbShiftPass.uniforms['amount'].value = 0.00010;
+composer.addPass(rgbShiftPass);
+
 // Controls
 const controls = new OrbitControls(camera, renderer.domElement);
-controls.enableDamping = true; // to make the camera move smoothly
-
-// render
-renderer.render(scene, camera);
+controls.enableDamping = true;
 
 function animate() {
-  window.requestAnimationFrame(animate); // to render the scene continuously
+  window.requestAnimationFrame(animate);
   controls.update();
   
   if (model) {
-    model.rotation.y += 0.005;
+    // model.rotation.y += 0.005;
   }
   
-  renderer.render(scene, camera);
+  composer.render();
 }
 
 animate();
